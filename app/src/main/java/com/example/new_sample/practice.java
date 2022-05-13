13ee0d0e500380904e20a00246a6fe3e113df720
Wahida Mahjabin <oro.wahida@gmail.com>
2022-05-13 14:34:50 +0600

    java class

---
+++ app/src/main/java/com/example/new_sample/practice.java
@@ -0,0 +1,18 @@
+package com.example.new_sample;
+
+import android.widget.Button;
+
+public class practice {
+
+    private String num;
+    public practice() {
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+}
