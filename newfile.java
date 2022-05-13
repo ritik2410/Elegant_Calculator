319d723d6667079d1aae3150658f4958ca6b51ae
Wahida Mahjabin <oro.wahida@gmail.com>
2022-05-14 03:16:53 +0600

    java file with constructor,getter and setter

---
+++ newfile.java
@@ -0,0 +1,17 @@
+package com.example.new_sample;
+
+public class newfile {
+    private String number;
+
+    public k(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
