b34f2ced2019b3419179f8e502fabd926c4e1a26
Wahida Mahjabin <oro.wahida@gmail.com>
2022-05-14 03:20:21 +0600

    branch java file added

---
+++ newbranchfile.java
@@ -0,0 +1,14 @@
+package com.example.new_sample;
+
+public class newbranchfile {
+    private String number;
+ 
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
