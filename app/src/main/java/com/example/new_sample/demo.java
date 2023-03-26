6c8cdb7aa7041c3f001240a2d6de98a1a246884c
topu <ardamax646@gmail.com>
2023-03-26 22:08:09 +0600

    added java class

---
+++ app/src/main/java/com/example/new_sample/demo.java
@@ -0,0 +1,27 @@
+package com.example.new_sample;
+
+public class demo {
+
+    private int x,y;
+
+    public demo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
