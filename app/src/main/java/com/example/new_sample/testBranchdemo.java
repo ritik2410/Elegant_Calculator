a37a953ff29c23161bb3e12c186dc748c3f32c18
topu <ardamax646@gmail.com>
2023-03-26 22:22:35 +0600

    added new branch and new java class

---
+++ app/src/main/java/com/example/new_sample/testBranchdemo.java
@@ -0,0 +1,18 @@
+package com.example.new_sample;
+
+public class testBranchdemo {
+
+    private String s;
+
+    public testBranchdemo(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
